3952a58bfc35fa3bffd74ef10c2217d0aa3f9e71
Hadeel Obaid <hadeelobaid71@gmail.com>
2023-11-30 01:14:03 +0300

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,33 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
+
+const MyComponent = () => {
+  const headingStyle = {
+    color: 'firebrick',
+  };
+
+  return (
+    <div>
+      <h1 style={headingStyle}>Hello, World!</h1>
+    </div>
+  );
+};
+
+
+
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    <h1 style={{color: "firebrick"}}>
+      {name} is a Web Developer from {city}
+      </h1>
+  </div>
+  );
 }
 
 export default Home;
+
